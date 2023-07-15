5d112cfac7339e757c91c4e8c05a45d60c0055eb
HoàngHuy <138868537+shopaccrandom@users.noreply.github.com>
2023-07-15 14:59:54 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("ADMIN SẼ CẬP NHẬT ACC LIÊN TỤC MỖI NGÀY VÀ HIỆN TẠI CHƯA CÓ ACC FREE FIRE ") 
+
+
+
+setTimeout /* setInterval */ (function(){
+    alert(" WEB CHỈ HOẠT ĐỘNG TỪ T2 - T6  ")
+},2000)
+
+
+
+/*      */
